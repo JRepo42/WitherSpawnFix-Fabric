4f80f4a629afed76b4e3ff4ecb31ee8046a5ea12
JRepo42 <JRepo42@users.noreply.github.com>
2024-07-01 01:35:52 +0100

    Wither fix!

---
+++ src/main/java/joe/wither_spawn_fix/WitherSpawnFix.java
@@ -0,0 +1,14 @@
+package joe.wither_spawn_fix;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WitherSpawnFix implements ModInitializer {
+	public static final String MODID = "wither_spawn_fix";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/joe/wither_spawn_fix/mixin/WitherSpawnMixin.java
@@ -0,0 +1,32 @@
+package joe.wither_spawn_fix.mixin;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.block.WitherSkullBlock;
+import net.minecraft.block.pattern.BlockPattern;
+import net.minecraft.block.pattern.BlockPatternBuilder;
+import net.minecraft.block.pattern.CachedBlockPosition;
+import net.minecraft.predicate.block.BlockStatePredicate;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.server.MinecraftServer;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(WitherSkullBlock.class)
+public class WitherSpawnMixin {
+	@Shadow
+	private static @Nullable BlockPattern witherBossPattern;
+
+	@Inject(at = @At("HEAD"), cancellable = true, method = "getWitherBossPattern")
+	private static void witherSpawnFix$getWitherBossPattern(CallbackInfoReturnable<BlockPattern> cir) {
+		if (witherBossPattern == null) {
+			// Will see how isSolid() fares; seems to work well.
+			witherBossPattern = BlockPatternBuilder.start().aisle("^^^", "###", "~#~").where('#', pos -> pos.getBlockState().isIn(BlockTags.WITHER_SUMMON_BASE_BLOCKS)).where('^', CachedBlockPosition.matchesBlockState(BlockStatePredicate.forBlock(Blocks.WITHER_SKELETON_SKULL).or(BlockStatePredicate.forBlock(Blocks.WITHER_SKELETON_WALL_SKULL)))).where('~', pos -> !pos.getBlockState().isSolid()).build();
+		}
+		cir.setReturnValue(witherBossPattern);
+	}
+}
