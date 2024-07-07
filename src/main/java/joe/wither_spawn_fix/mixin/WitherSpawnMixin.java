b919a399583adf23b0976bafc50a487dadbef246
JRepo42 <JRepo42@users.noreply.github.com>
2024-07-07 01:17:10 +0100

    1.21 port!

--- src/main/java/joe/wither_spawn_fix/mixin/WitherSpawnMixin.java
+++ src/main/java/joe/wither_spawn_fix/mixin/WitherSpawnMixin.java
@@ -1,14 +1,12 @@
 package joe.wither_spawn_fix.mixin;
 
 import net.minecraft.block.Blocks;
-import net.minecraft.block.Material;
 import net.minecraft.block.WitherSkullBlock;
 import net.minecraft.block.pattern.BlockPattern;
 import net.minecraft.block.pattern.BlockPatternBuilder;
 import net.minecraft.block.pattern.CachedBlockPosition;
 import net.minecraft.predicate.block.BlockStatePredicate;
-import net.minecraft.tag.BlockTags;
-import net.minecraft.util.function.MaterialPredicate;
+import net.minecraft.registry.tag.BlockTags;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -23,11 +21,23 @@ public class WitherSpawnMixin {
 
 	@Inject(at = @At("HEAD"), cancellable = true, method = "getWitherBossPattern")
 	private static void witherSpawnFix$getWitherBossPattern(CallbackInfoReturnable<BlockPattern> cir) {
+		/*
 		if (witherBossPattern == null) {
 			witherBossPattern = BlockPatternBuilder.start().aisle(new String[]{"^^^", "###", "~#~"}).where('#', (pos) -> {
 				return pos.getBlockState().isIn(BlockTags.WITHER_SUMMON_BASE_BLOCKS);
 			}).where('^', CachedBlockPosition.matchesBlockState(BlockStatePredicate.forBlock(Blocks.WITHER_SKELETON_SKULL).or(BlockStatePredicate.forBlock(Blocks.WITHER_SKELETON_WALL_SKULL)))).where('~', pos -> !pos.getBlockState().getMaterial().isSolid()).build();
 		}
+
+		 */
+
+		if (witherBossPattern == null) {
+			witherBossPattern = BlockPatternBuilder.start().aisle(new String[]{"^^^", "###", "~#~"}).where('#', (pos) -> {
+				return pos.getBlockState().isIn(BlockTags.WITHER_SUMMON_BASE_BLOCKS);
+			}).where('^', CachedBlockPosition.matchesBlockState(BlockStatePredicate.forBlock(Blocks.WITHER_SKELETON_SKULL).or(BlockStatePredicate.forBlock(Blocks.WITHER_SKELETON_WALL_SKULL)))).where('~', (pos) -> {
+				return !pos.getBlockState().isSolid();
+			}).build();
+		}
+
 		cir.setReturnValue(witherBossPattern);
 	}
 }
